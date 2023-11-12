bea0ff1c2d125b2a1b08ff63e13276f78d272099
shihuojian <shihuojian@hotmail.com>
2023-11-12 11:04:39 +0800

    add style scss

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,4 +1,4 @@
-
-export default function App() {
-    return <div>testsdfsdfsdfsdf</div>
+import style from'./style.module.scss';
+export default function App() { 
+    return <div className={style.test}>Hello, Next.js!</div>
 }

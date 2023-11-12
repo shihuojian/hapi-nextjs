20c7c395d6ade913303cc06c5a7937e76693805d
shihuojian <shihuojian@hotmail.com>
2023-11-12 13:45:58 +0800

    sdf

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -3,6 +3,7 @@ export default function App() {
     return (
         <div className={style.test}>
             <h1>Hello, Next.js!</h1>
+            <test>tt</test>
             <div className='test1'>test1</div>
         </div>
     )
